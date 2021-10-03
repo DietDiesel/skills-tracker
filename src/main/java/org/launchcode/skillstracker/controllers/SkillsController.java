ad145bf7b0ef49db8ff77ac2378df53fa9c8ab41
swankidelic <swankidelic@gmail.com>
2021-10-02 21:58:58 -0500

    completing studio

--- src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
+++ src/main/java/org/launchcode/skillstracker/controllers/SkillsController.java
@@ -16,43 +16,43 @@ public class SkillsController {
                       "<html lang='en'>\n" +
                       "<head>\n" +
                       "    <meta charset='UTF-8'>\n" +
-                      "    <title>Title</title>\n" +
+                      "    <title>Fave Lang Form Exercise</title>\n" +
                       "</head>\n" +
                       "<body>\n" +
                       "<div style='display:block; margin-left:auto; margin-right:auto; width:25%;'>\n";
 
-    String htmlResponseTable1 = "<div style='width=100%;'>\n<h3>Name:</h3>\n<h2>";
+    String htmlResponseTable1 = "<div style='width=100%; display:flex; flex-direction: column;'>\n<h3>Name:</h3>\n<h2>";
     String htmlResponseTable2 = "</h2>\n<h3>My favorite language:</h3>\n<h2>";
     String htmlResponseTable3 = "</h2>\n<h3>This one is cool too:</h3>\n<h2>";
     String htmlResponseTable4 = "</h2>\n<h3>This one... well, it doesn't suck:</h3>\n<h2>";
     String htmlResponseTable5 = "</h2>\n</div>\n";
 
-    String htmlForm ="    <div style='width=100%;'>\n" +
-            "        <form method=\"get\" action=\"/form\">\n" +
-            "            <label for='name'>Enter your name here:</label><br>\n" +
-            "            <input type='text' id='name' name='name'><br>\n" +
-            "            <label for='lang1'>Choose your favorite language:</label><br>\n" +
-            "            <select id='lang1' name='lang1'>\n" +
-            "                <option value='JavaScript'>JavaScript</option>\n" +
-            "                <option value='Java'>Java</option>\n" +
-            "                <option value='Python'>Python</option>\n" +
-            "                <option value='Perl'>Perl</option>\n" +
-            "            </select><br>\n" +
-            "            <select id='lang2' name='lang2'>\n" +
-            "                <option value='JavaScript'>JavaScript</option>\n" +
-            "                <option value='Java'>Java</option>\n" +
-            "                <option value='Python'>Python</option>\n" +
-            "                <option value='Perl'>Perl</option>\n" +
-            "            </select><br>\n" +
-            "            <select id='lang3' name='lang3'>\n" +
-            "                <option value='JavaScript'>JavaScript</option>\n" +
-            "                <option value='Java'>Java</option>\n" +
-            "                <option value='Python'>Python</option>\n" +
-            "                <option value='Perl'>Perl</option>\n" +
-            "            </select><br>\n" +
-            "            <input type='submit' value='Lets go!'>\n" +
-            "        </form>\n" +
-            "    </div>\n";
+    String htmlForm = "    <div style='width=100%; display:flex; flex-direction: column;'>\n" +
+                    "        <form method=\"get\" action=\"/form\">\n" +
+                    "            <label for='name'>Enter your name here:</label><br>\n" +
+                    "            <input type='text' id='name' name='name'><br>\n" +
+                    "            <label for='lang1'>Choose your favorite language:</label><br>\n" +
+                    "            <select id='lang1' name='lang1'>\n" +
+                    "                <option value='JavaScript'>JavaScript</option>\n" +
+                    "                <option value='Java'>Java</option>\n" +
+                    "                <option value='Python'>Python</option>\n" +
+                    "                <option value='Perl'>Perl</option>\n" +
+                    "            </select><br>\n" +
+                    "            <select id='lang2' name='lang2'>\n" +
+                    "                <option value='JavaScript'>JavaScript</option>\n" +
+                    "                <option value='Java'>Java</option>\n" +
+                    "                <option value='Python'>Python</option>\n" +
+                    "                <option value='Perl'>Perl</option>\n" +
+                    "            </select><br>\n" +
+                    "            <select id='lang3' name='lang3'>\n" +
+                    "                <option value='JavaScript'>JavaScript</option>\n" +
+                    "                <option value='Java'>Java</option>\n" +
+                    "                <option value='Python'>Python</option>\n" +
+                    "                <option value='Perl'>Perl</option>\n" +
+                    "            </select><br>\n" +
+                    "            <input type='submit' value='Lets go!'>\n" +
+                    "        </form>\n" +
+                    "    </div>\n";
 
     String htmlEnd = "</div>\n</body>\n</html>";
 
